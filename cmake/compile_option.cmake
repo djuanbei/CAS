75b7203e4d41e1095575a1a5ac6e80bbabf86a57
yundai <dlyun2009@gmail.com>
2024-09-21 08:59:04 +0800

    Add require header files

--- cmake/compile_option.cmake
+++ cmake/compile_option.cmake
@@ -1,11 +1,12 @@
 
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -g")
+
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_compile_definitions(_GLIBCXX_DEBUG)
 endif ()
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -g")
     message(STATUS "Using GCC")
     # Add strict compiler flags
     add_compile_options(
@@ -26,6 +27,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
     )
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -g")
     message(STATUS "Using Clang")
     # Add strict compiler flags
     add_compile_options(
