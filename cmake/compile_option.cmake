c328d11bd6a7f7bdcea70dda7c056529094d5544
yundai <dlyun2009@gmail.com>
2024-09-21 08:52:54 +0800

    For diff compiler using different compile options

--- cmake/compile_option.cmake
+++ cmake/compile_option.cmake
@@ -4,21 +4,49 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -g")
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_compile_definitions(_GLIBCXX_DEBUG)
 endif ()
-# Add strict compiler flags
-add_compile_options(
-        -Wall
-        -Wextra
-        -Wpedantic
-        -Werror
-        -Wstrict-aliasing
-        -Wformat=2
-        -Wshadow
-        -Wconversion
-        #-Wsign-conversion
-        -Wstrict-overflow=5
-        -Wundef
-        #-Wno-unused
-        -Wno-sign-compare
-        -Wno-sign-conversion
 
-)
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    message(STATUS "Using GCC")
+    # Add strict compiler flags
+    add_compile_options(
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Werror
+            -Wstrict-aliasing
+            -Wformat=2
+            -Wshadow
+            -Wconversion
+            #-Wsign-conversion
+            -Wstrict-overflow=5
+            -Wundef
+            #-Wno-unused
+            -Wno-sign-compare
+            -Wno-sign-conversion
+
+    )
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    message(STATUS "Using Clang")
+    # Add strict compiler flags
+    add_compile_options(
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Werror
+            -Wstrict-aliasing
+            -Wformat=2
+            -Wshadow
+            -Wconversion
+            #-Wsign-conversion
+            -Wstrict-overflow=5
+            -Wundef
+            #-Wno-unused
+            -Wno-sign-compare
+            -Wno-sign-conversion
+
+    )
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    add_compile_options(/W4 /WX /permissive- /fp:strict /analyze /we4244 /we4018 /wd4018 /wd4365)
+
+endif ()
+
